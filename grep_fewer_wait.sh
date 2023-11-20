909150559013dc1ed7b484649264977e7bda10b3
Jarvis Jia <jia44@wisc.edu>
2023-11-20 02:26:27 -0600

    Update the gpu_test with fewer wait version

---
+++ grep_fewer_wait.sh
@@ -0,0 +1,52 @@
+echo fifo
+grep -R ": GPU Cache" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/fifo_test_1.txt | wc -l
+grep -R ": GPU Cache hit" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/fifo_test_1.txt | wc -l
+grep -R ": GPU Cache" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/fifo_test_2.txt  | wc -l
+grep -R ": GPU Cache hit" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/fifo_test_2.txt  | wc -l
+echo lfu
+grep -R ": GPU Cache" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/lfu_test_1.txt  | wc -l
+grep -R ": GPU Cache hit" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/lfu_test_1.txt  | wc -l
+grep -R ": GPU Cache" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/lfu_test_2.txt | wc -l
+grep -R ": GPU Cache hit" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/lfu_test_2.txt | wc -l
+grep -R ": GPU Cache" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/lfu_test_3.txt  | wc -l
+grep -R ": GPU Cache hit" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/lfu_test_3.txt  | wc -l
+echo lip
+grep -R ": GPU Cache" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/lip_test_1.txt  | wc -l
+grep -R ": GPU Cache hit" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/lip_test_1.txt  | wc -l
+echo lru
+grep -R ": GPU Cache" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/lru_test_1.txt  | wc -l
+grep -R ": GPU Cache hit" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/lru_test_1.txt  | wc -l
+grep -R ": GPU Cache" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/lru_test_2.txt  | wc -l
+grep -R ": GPU Cache hit" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/lru_test_2.txt  | wc -l
+grep -R ": GPU Cache" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/lru_test_3.txt  | wc -l
+grep -R ": GPU Cache hit" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/lru_test_3.txt  | wc -l
+grep -R ": GPU Cache" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/lru_test_4.txt  | wc -l
+grep -R ": GPU Cache hit" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/lru_test_4.txt  | wc -l
+echo mru
+grep -R ": GPU Cache" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/mru_test_1.txt  | wc -l
+grep -R ": GPU Cache hit" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/mru_test_1.txt  | wc -l
+grep -R ": GPU Cache" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/mru_test_2.txt  | wc -l
+grep -R ": GPU Cache hit" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/mru_test_2.txt  | wc -l
+echo nru
+grep -R ": GPU Cache" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/nru_test_1.txt  | wc -l
+grep -R ": GPU Cache hit" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/nru_test_1.txt  | wc -l
+echo rrip
+grep -R ": GPU Cache" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/rrip_test_1.txt  | wc -l
+grep -R ": GPU Cache hit" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/rrip_test_1.txt  | wc -l
+grep -R ": GPU Cache" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/rrip_test_2.txt  | wc -l
+grep -R ": GPU Cache hit" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/rrip_test_2.txt  | wc -l
+echo second_chance_test
+grep -R ": GPU Cache" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/second_chance_test_1.txt  | wc -l
+grep -R ": GPU Cache hit" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/second_chance_test_1.txt  | wc -l
+grep -R ": GPU Cache" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/second_chance_test_2.txt  | wc -l
+grep -R ": GPU Cache hit" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/second_chance_test_2.txt  | wc -l
+grep -R ": GPU Cache" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/second_chance_test_3.txt  | wc -l
+grep -R ": GPU Cache hit" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/second_chance_test_3.txt  | wc -l
+echo tree
+grep -R ": GPU Cache" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/tree_plru_test_1.txt  | wc -l
+grep -R ": GPU Cache hit" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/tree_plru_test_1.txt  | wc -l
+grep -R ": GPU Cache" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/tree_plru_test_2.txt  | wc -l
+grep -R ": GPU Cache hit" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/tree_plru_test_2.txt  | wc -l
+grep -R ": GPU Cache" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/tree_plru_test_3.txt  | wc -l
+grep -R ": GPU Cache hit" /u/j/a/jarvis/research/gem5/hit_result_fewer_wait/tree_plru_test_3.txt  | wc -l
+
